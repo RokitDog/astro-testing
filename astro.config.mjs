924def55fec3c0c35a991f72c17c7011b3a72335
Rokitdog <bojanforgaming@hotmail.com>
2022-11-15 23:34:07 +0100

    serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/edge'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 import react from '@astrojs/react'
